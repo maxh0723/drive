537794da0475b2b871914eb76344cbdf69716e26
Erik Hansen <erik@whatifmediagroup.com>
2019-12-05 01:52:34 -0500

    Updates

--- GUI.java
+++ GUI.java
@@ -6,22 +6,33 @@ import BreezySwing.*;
 public class GUI extends GBFrame {
 
 	JButton addBook = addButton("Add Book", 3, 1, 1, 1);
-	JButton loanBook = addButton("Loan Book", 3, 2, 1, 1);
+	JButton removeBook = addButton("Remove Book", 3, 2, 1, 1);
+	JButton loanBook = addButton("Loan Book", 3, 3, 1, 1);
+	JButton returnBook = addButton("Return Book", 3, 4, 1, 1);
 
 	JTextField dateField = addTextField("Enter Date, format DD/MM/YYYY", 1, 1, 1, 1);
 	JTextField titleField = addTextField("Enter Title Here", 2, 1, 1, 1);
 	JTextField authorField = addTextField("Enter Author Here", 2, 2, 1, 1);
 
-	Book book = new Book();
-	ArrayList<Book> books = new ArrayList<Book>();
+	// Default (start with empty library)
+	// Library library = new Library();
 
+	// Debug (preseed library with books)
+	Library library = new Library(true);
+	
 	public void buttonClicked(JButton buttonObj) {
 		if (buttonObj == addBook) {
-			AddDialog add = new AddDialog(this, books);
+			AddDialog add = new AddDialog(this, library);
 			add.setVisible(true);
 		} else if (buttonObj == loanBook) {
-			LoanDialog loan = new LoanDialog(this, books);
+			LoanDialog loan = new LoanDialog(this, library);
 			loan.setVisible(true);
+		} else if (buttonObj == returnBook) {
+			ReturnDialog returnDialog = new ReturnDialog(this, library);
+			returnDialog.setVisible(true);
+		} else if(buttonObj == removeBook) {
+			RemoveDialog remove = new RemoveDialog(this, library);
+			remove.setVisible(true);
 		}
 	}
 
---
+++ LibraryException.java
@@ -0,0 +1,11 @@
+
+public class LibraryException extends Exception {
+
+	public LibraryException(String message) {
+		super(message);
+	}
+	
+	public LibraryException(String message, Throwable t) {
+		super(message, t);
+	}
+}
